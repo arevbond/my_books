7bd4b74e755b18ef710e6010fe049e08dd72b583
arevbond <bondarev.n17@yandex.ru>
2022-10-19 13:02:25 +0300

    add: app Book - model Book

---
+++ mybooks/book/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+# from mybooks.users.models import User
+User = get_user_model()
+
+
+class Category(models.TextChoices):
+    artistic = 'artistic', 'Художественная литература'
+    non_artistic = 'non_artistic', 'Не художественная литература'
+
+
+class Book(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    title = models.CharField(max_length=255, verbose_name='Название книги')
+    author = models.CharField(max_length=100, verbose_name='Автор книги')
+    category = models.CharField(max_length=50, choices=Category.choices, verbose_name='Категория')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Обложка', blank=True, null=True)
+    review = models.TextField(verbose_name='Рецензия')
+    rating = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(10.0)],
+                               verbose_name='Оценка')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создание')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
+    favorite = models.BooleanField(default=False, verbose_name='Любимая книга')
+
+    def __str__(self):
+        return f'{self.user} - {self.title}'
--- mybooks/mybooks/settings.py
+++ mybooks/mybooks/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'users.apps.UsersConfig',
+    'users.apps.UsersConfig',   # приложение с пользователями
+    'book.apps.BookConfig',     # приложение с книгами
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDUA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
